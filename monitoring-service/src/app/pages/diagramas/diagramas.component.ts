4156d223edaa3e48f9c7622ba2807830d120e32d
BryanSagbay <bryansagbay2001@hotmail.com>
2024-12-26 14:15:29 -0500

    consume api with service

--- monitoring-service/src/app/pages/diagramas/diagramas.component.ts
+++ monitoring-service/src/app/pages/diagramas/diagramas.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DbConexionService } from '../../service/db-conexion.service';
+import { Datos } from '../../models/Datos';
+import {
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-diagramas',
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './diagramas.component.html',
   styleUrl: './diagramas.component.css'
 })
 export class DiagramasComponent {
+
+  dato: Datos = new Datos();
+  bandera: boolean=false;
+  datos: Datos[] = [];
+constructor(private DbConexionService:DbConexionService){
+
+}
+ngOnInit(){
+this.DbConexionService.getAllData().subscribe( (data:Datos[]) => {
+this.datos = data
+})
+this.dato=new Datos();
+}
+MostrarDatos(){
+  if(this.bandera==false){
+  this.DbConexionService.getAllData().subscribe( (data:Datos[]) => {
+    this.datos = data
+  })
+  this.dato=new Datos();
+}}
+
+
 
 }
--- monitoring-service/src/app/service/db-conexion.service.ts
+++ monitoring-service/src/app/service/db-conexion.service.ts
@@ -1,23 +1,35 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Datos } from '../models/Datos';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DbConexionService {
 
-  private url = 'http://localhost:4000';
+  private url = 'http://localhost:4001';
 
   constructor(private http: HttpClient) { }
 
-  //Metodo Get obtenemos todos los datos de la base de datos
-  getAllData(){
-    return this.http.get(`${this.url}/pcws`);
+  // Método Get para obtener todos los datos de la base de datos
+  getAllData(): Observable<Datos[]> {
+    return this.http.get<Datos[]>(`${this.url}/pcws`).pipe(
+      catchError(this.handleError)
+    );
   }
 
-  //Metodo Get obtenemos los datos de la base de datos por id
-  getDataId(id: number): Observable<any> {
-    return this.http.get(`${this.url}/pcws/${id}`);
+  // Método Get para obtener los datos de la base de datos por id
+  getDataId(id: number): Observable<Datos> {
+    return this.http.get<Datos>(`${this.url}/pcws/${id}`).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  // Método para manejar errores
+  private handleError(error: HttpErrorResponse) {
+    console.error('Ocurrió un error:', error.message);
+    return throwError('Algo salió mal; por favor intenta nuevamente más tarde.');
   }
 }
