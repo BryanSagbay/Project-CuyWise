4cc9c6671cd470d1a1306d3c29583fcaa8442fe6
BryanSagbay <bryansagbay2001@hotmail.com>
2024-12-26 14:14:22 -0500

    add model

---
+++ monitoring-service/src/app/models/Datos.ts
@@ -0,0 +1,18 @@
+export class Datos {
+    id: number = 0;
+    animal_id: number = 0;
+    nombre_animal: string = '';
+    raza_animal: string = '';
+    fecha_registro: string = '';
+    activo_animal: boolean = false;
+    medicion_id: number = 0;
+    peso_medicion: number = 0;
+    imagen_medicion: string = '';
+    fecha_medicion: string = '';
+    evento_id: number = 0;
+    tipo_evento: string = '';
+    descripcion: string = '';
+    fecha_evento: string = '';
+    
+
+}
