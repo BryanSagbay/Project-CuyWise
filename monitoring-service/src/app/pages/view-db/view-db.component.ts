6b715ce10c707894940d95e565446e18b2568a58
BryanSagbay <bryansagbay2001@hotmail.com>
2024-12-26 10:48:07 -0500

    add new section

---
+++ monitoring-service/src/app/pages/view-db/view-db.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-db',
+  imports: [],
+  templateUrl: './view-db.component.html',
+  styleUrl: './view-db.component.css'
+})
+export class ViewDBComponent {
+
+}
