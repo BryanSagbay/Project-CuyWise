07cc21f0bc3206761b585542ceec35c89751abbf
BryanSagbay <bryansagbay2001@hotmail.com>
2024-12-27 09:00:50 -0500

    change class to interface

--- monitoring-service/src/app/models/Eventos.ts
+++ monitoring-service/src/app/models/Eventos.ts
@@ -1,7 +1,7 @@
-export class Eventos {
-    id: number = 0;
-    evento_id: number = 0;
-    tipo_evento: string = '';
-    descripcion_evento: string = '';
-    fecha_evento: string = '';
+export interface Eventos {
+    id: number;
+    animal_id: number;
+    tipo_evento: string;
+    descripcion: string;
+    fecha_evento: string;
 }
