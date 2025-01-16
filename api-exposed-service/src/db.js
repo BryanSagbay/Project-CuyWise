e85e5bf45fadbe268d7f4246d78f014e7fac001b
BryanSagbay <bryansagbay2001@hotmail.com>
2025-01-15 22:52:27 -0500

    update documentacion api

--- api-exposed-service/src/db.js
+++ api-exposed-service/src/db.js
@@ -3,6 +3,7 @@ import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from './config.js';
 
 export const pool = new pg.Pool({
 
+    // Connection to the database
     user: DB_USER,
     host: DB_HOST,
     database: DB_NAME,
