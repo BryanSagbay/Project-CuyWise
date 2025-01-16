ddd0704bb891c628ddbdf60b4d6243601ffa2a02
BryanSagbay <bryansagbay2001@hotmail.com>
2025-01-15 22:53:55 -0500

    update documentacion api

--- api-exposed-service/src/index.js
+++ api-exposed-service/src/index.js
@@ -14,6 +14,7 @@ app.use(cors());
 app.use(pcwRoutes);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
+// Start the server
 app.listen(PORT, () => {
   console.log('Server is running on http://localhost:'+ PORT);
 }); 
