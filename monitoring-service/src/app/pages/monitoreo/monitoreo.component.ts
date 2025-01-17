585783112953c7cdee8eac300efe9e688557fd32
BryanSagbay <bryansagbay2001@hotmail.com>
2025-01-17 10:25:29 -0500

    edit scripts and add database structure

--- monitoring-service/src/app/pages/monitoreo/monitoreo.component.ts
+++ monitoring-service/src/app/pages/monitoreo/monitoreo.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, ElementRef, ViewChild } from '@angular/core';
 import { CameraService } from '../../service/camera.service';
 
 @Component({
@@ -9,23 +9,38 @@ import { CameraService } from '../../service/camera.service';
 })
 export class MonitoreoComponent {
 
+  @ViewChild('videoElement') videoElement!: ElementRef<HTMLVideoElement>;
+  mediaStream!: MediaStream; 
+
   constructor(private cameraService: CameraService){}
 
-  // Funciones para iniciar y detener la cámara
-  start() {
-    this.cameraService.startCamera().subscribe(response => {
-      console.log('Cámara iniciada:', response);
-    }, error => {
-      console.error('Error al iniciar la cámara:', error);
-    });
+  ngOnInit(): void {}
+
+  ngOnDestroy(): void {
+    this.stop();
   }
 
-  stop() {
-    this.cameraService.stopCamera().subscribe(response => {
-      console.log('Cámara detenida:', response);
-    }, error => {
-      console.error('Error al detener la cámara:', error);
-    });
+  start(): void {
+    // Accede a la cámara usando getUserMedia
+    navigator.mediaDevices
+      .getUserMedia({ video: true }) // Configura solo video, sin audio
+      .then((stream) => {
+        this.mediaStream = stream;
+        const video = this.videoElement.nativeElement;
+        video.srcObject = stream; // Asigna el stream al elemento video
+        video.play(); // Reproduce el video
+      })
+      .catch((error) => {
+        console.error('Error al acceder a la cámara:', error);
+      });
+  }
+
+  stop(): void {
+    if (this.mediaStream) {
+      // Detén todas las pistas del stream
+      this.mediaStream.getTracks().forEach((track) => track.stop());
+      this.mediaStream = null!;
+    }
   }
 }
 
