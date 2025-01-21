52562cfe48da9ed140ef0978ee7a02d03b97996c
BryanSagbay <bryansagbay2001@hotmail.com>
2025-01-21 09:54:43 -0500

    update comments

--- object-detection-service/src/flask/service_camera.py
+++ object-detection-service/src/flask/service_camera.py
@@ -1,10 +1,11 @@
 # Description: This file is used to test the YOLO model with the camera.
 from ultralytics import YOLO
 
-model = YOLO('../models/model_clasification.pt')
+model = YOLO('/home/bryan/Documents/Proyects/Project-CuyWise/object-detection-service/src/models/model_clasification.pt')
 
 results = model.predict(source=0, show=True)
 
 print(results)
 
+# cerrar
 model.close()
