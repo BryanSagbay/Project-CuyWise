c4bf2fb31790dd828ddd4a8d2666b080d6133b3d
BryanSagbay <bryansagbay2001@hotmail.com>
2025-01-22 16:20:18 -0500

    update

--- object-detection-service/src/flask/weight.py
+++ object-detection-service/src/flask/weight.py
@@ -3,7 +3,6 @@ from hx711 import HX711
 # Función para detectar peso con el módulo HX711
 def medir_peso():
     try:
-        # Inicializar el sensor HX711 (ajusta los pines según tu configuración)
         hx = HX711(dout_pin=29, pd_sck_pin=31)
         hx.set_scale(7050)  # Ajustar escala según tu calibración
         hx.tare()  # Tarar la balanza antes de medir
