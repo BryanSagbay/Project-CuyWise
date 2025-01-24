28ee736e15ad9b1468de1f6bf683074aa0a23319
BryanSagbay <bryansagbay2001@hotmail.com>
2025-01-24 10:01:42 -0500

    update

--- object-detection-service/src/flask/service_camera.py
+++ object-detection-service/src/flask/service_camera.py
@@ -8,7 +8,6 @@ from ultralytics import YOLO
 # Función principal con YOLO
 def model_YOLO():
     try:
-        # Cargar el modelo YOLO
         model = YOLO('/home/bryan/Documents/Proyects/Project-CuyWise/object-detection-service/src/models/model_clasification.pt')
 
         # Iniciar captura de video
