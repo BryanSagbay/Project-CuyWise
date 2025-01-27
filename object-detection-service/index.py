af835936d2c433baf0b1ed5e21158ab9ef897654
BryanSagbay <bryansagbay2001@hotmail.com>
2025-01-27 11:29:24 -0500

    update comments

--- object-detection-service/index.py
+++ object-detection-service/index.py
@@ -5,7 +5,7 @@ from src.flask.service_camera import model_YOLO
 #from src.services.service_weight import run_weight_service
 
 def main():
-    # Carga variables de entorno
+    Carga variables de entorno
     load_dotenv()
 
     print("Iniciando el sistema...")
