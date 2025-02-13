be5cc517c727d58ad9141319df4849b907bdf5eb
Bryan Sagbay <85771422+BryanSagbay@users.noreply.github.com>
2025-02-13 09:07:15 -0500

    Update __init__.py

--- object-detection-service/src/services/__init__.py
+++ object-detection-service/src/services/__init__.py
@@ -22,11 +22,4 @@ def detectar_cuyes():
                 confidence = box.conf[0].item()
                 cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
                 cv2.putText(frame, f'{label}: {confidence:.2f}', (x1, y1 - 10),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-                
-                print(f"{result.idx}: {frame.shape[0]}x{frame.shape[1]} 1 {label}, {confidence:.2f}")
-                peso = obtener_peso()
-                print(f"Peso detectado: {peso}g")
-                
-                animal_id = obtener_id_animal(label
-         
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)    
