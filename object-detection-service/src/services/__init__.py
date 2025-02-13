70f66e983aea6c361a12288a5805af11289855c0
Bryan Sagbay <85771422+BryanSagbay@users.noreply.github.com>
2025-02-13 09:07:26 -0500

    Update __init__.py

--- object-detection-service/src/services/__init__.py
+++ object-detection-service/src/services/__init__.py
@@ -15,7 +15,3 @@ def detectar_cuyes():
             break
         
         results = model(frame)
-        for result in results:
-            for box in result.boxes:
-                x1, y1, x2, y2 = map(int, box.xyxy[0])
-                label = result.names[int(box.cls)]
