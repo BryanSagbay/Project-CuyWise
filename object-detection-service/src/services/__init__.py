901e75cd96bb9ad076b7c17163b41a06b7a7ddf6
Bryan Sagbay <85771422+BryanSagbay@users.noreply.github.com>
2025-02-13 09:07:36 -0500

    Update __init__.py

--- object-detection-service/src/services/__init__.py
+++ object-detection-service/src/services/__init__.py
@@ -7,7 +7,4 @@ from database import registrar_medicion, registrar_evento, obtener_id_animal
 
 def detectar_cuyes():
     model = YOLO('models/classification.pt')
-    cap = cv2.VideoCapture(0)
-    
-    while cap.isOpened():
-        success, frame = cap.read()
+
