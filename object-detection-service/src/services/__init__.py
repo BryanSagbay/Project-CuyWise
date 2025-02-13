ab00c5ea764737213037906ec2a7f21d0c4c9062
Bryan Sagbay <85771422+BryanSagbay@users.noreply.github.com>
2025-02-13 09:07:42 -0500

    Update __init__.py

--- object-detection-service/src/services/__init__.py
+++ object-detection-service/src/services/__init__.py
@@ -2,9 +2,3 @@
 # detector.py
 from ultralytics import YOLO
 import cv2
-from balanza import obtener_peso
-from database import registrar_medicion, registrar_evento, obtener_id_animal
-
-def detectar_cuyes():
-    model = YOLO('models/classification.pt')
-
